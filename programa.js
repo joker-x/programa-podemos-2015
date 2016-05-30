f59a4822cea015a54fc73115bcdd4c5a0169f9fc
Iván Eixarch <ivan@sinanimodelucro.org>
2016-05-30 13:19:59 +0200

    Añadiendo páginas fakes para compartir en facebook

--- programa.js
+++ programa.js
@@ -5,6 +5,7 @@ var fs = require('fs') // IO ficheros
   , _ = require ('lodash') // funciones manipulación de colecciones
   , ejs = require ('ejs') // motor de plantillas
   , marked = require ('marked') // renderiza markdown
+  , html2text = require('html-to-text') //convierte html a texto formateado
   , minify = require('html-minifier').minify; // reducir html
 var locales = ['es', 'en', 'ca', 'ast', 'eu', 'gl'];
 var i18n = new (require('i18n-2'))({
@@ -19,6 +20,7 @@ locales.forEach(function(idioma) {
 	var FICH_DATA = __dirname + '/data/programa_'+idioma+'.csv';
 	var DELIMITER = '\t';
 	var PLANTILLA = __dirname + '/plantilla-wp-cat.ejs';
+	var PLANTILLA_FB = __dirname + '/plantilla-fb-share.ejs';
 
 	var etiquetas = require(__dirname + '/data/etiquetas.json');
 	//añadimos slugs
@@ -44,12 +46,14 @@ locales.forEach(function(idioma) {
 		for (var i=1; i<total_medidas; i++) {
 			var num = parseInt(medidas_csv[i][0]);
 			if (num) {
+				var descripcion = (medidas_csv[i][3].indexOf('</') == -1)?marked(medidas_csv[i][3]):medidas_csv[i][3];
 				var medida = {
 					num: num,
 					eje: (medidas_csv[i][1])?medidas_csv[i][1]:"",
 					titulo: (medidas_csv[i][2])?medidas_csv[i][2]:"",
 					//descripcion: marked(medidas_csv[i][3]),
-					descripcion: (medidas_csv[i][3].indexOf('</') == -1)?marked(medidas_csv[i][3]):medidas_csv[i][3],
+					descripcion: descripcion,
+					descripcion_text: html2text.fromString(descripcion),
 					etiquetas: dame_etiquetas(num)
 				};
 				medidas.push(medida);
@@ -69,8 +73,11 @@ locales.forEach(function(idioma) {
 		fs.writeFileSync(__dirname+'/web/programa_'+idioma+'.min.html', minify(pagina, { 
 			collapseWhitespace: true, 
 			removeAttributeQuotes: true }));
-
-console.log(idioma);
+		_.each (medidas, function(medida) {
+			pagina = ejs.render(fs.readFileSync(PLANTILLA_FB, "utf8"), {medida: medida, i18n: i18n, idioma: idioma});
+			fs.writeFileSync(__dirname+'/web/fb-share/'+idioma+'/'+medida.num+'.html', pagina);
+		});
+		console.log(idioma);
 
 	});
 
